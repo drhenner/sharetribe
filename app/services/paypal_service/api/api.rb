e2690b142ba22df2b08aece3cf83cb92cca9b78c
Mikko Koski <mikkokos@gmail.com>
2016-08-29 18:03:46 +0300

    Don't use Fakepal

--- app/services/paypal_service/api/api.rb
+++ app/services/paypal_service/api/api.rb
@@ -5,7 +5,6 @@ module PaypalService::API
     if Rails.env.test?
       FakeApiImplementation
     else
-      FakeApiImplementation
-      # ApiImplementation
+      ApiImplementation
     end
 end
